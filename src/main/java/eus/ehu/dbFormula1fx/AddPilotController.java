5fd8fc3aad3909f361584d81d4677007938ded2c
Juanan Pereira <juanan.pereira@ehu.eus>
2023-02-14 22:36:23 +0100

    Scaffolding for adding a new pilot

---
+++ src/main/java/eus/ehu/dbFormula1fx/AddPilotController.java
@@ -0,0 +1,29 @@
+package eus.ehu.dbFormula1fx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class AddPilotController {
+
+    @FXML
+    private ComboBox<String> country;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField points;
+
+    @FXML
+    void addAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
